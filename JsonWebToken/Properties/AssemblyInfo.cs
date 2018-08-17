f43b8de175d668d671c47f6bc515d900707ed1ef
Vinicius de Melo Rocha <vmrocha@gmail.com>
2018-08-16 22:43:41 -0400

    add nuspec file

--- JsonWebToken/Properties/AssemblyInfo.cs
+++ JsonWebToken/Properties/AssemblyInfo.cs
@@ -5,14 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("JsonWebToken")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyTitle("Json Web Token")]
+[assembly: AssemblyDescription("JWT - Json Web Token for the .NET Framework")]
+[assembly: AssemblyCompany("Vinicius de Melo Rocha")]
 [assembly: AssemblyProduct("JsonWebToken")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
+[assembly: AssemblyCopyright("Copyright © 2018")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
